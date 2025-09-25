d22e6ad2fe32fdbf67b3e0a61b9067ea10adecc4
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-25 15:35:31 -0500

    sample1

--- catalogue.sh
+++ catalogue.sh
@@ -37,7 +37,7 @@ dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "installing nodejs"
 
 id roboshop &>>$LOG_FILE
-if[ $? -ne 0];then  
+if [ $? -ne 0];then  
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
 else
     echo -e"roboshop user already exist....$Y skipping $N"
