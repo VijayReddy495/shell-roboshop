3b4f1ded28f5c53b39ba22aafb75cb7fb03bf5b2
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-26 10:35:44 -0500

    sample1

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-SCRIPT_DIR=$pwd
+SCRIPT_DIR=$PWD
 echo "current directory is : $SCRIPT_DIR "
 MONGODB_HOST=mongodb.daws86s.fun
 mkdir -p $LOGS_FOLDER
